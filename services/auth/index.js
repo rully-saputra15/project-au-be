70881a2f8f51209e2cd511a5c020216955f6e5e2
mrizalrizky <61452014+mrizalrizky@users.noreply.github.com>
2024-05-07 09:41:16 +0700

    fix: remove unused params and passwordUtils import

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,20 +1,12 @@
 import { jwtEncode } from '../../utils/jwtUtils';
-import { comparePassword, hashPassword } from '../../utils/passwordUtils';
 import SupabaseClient from '../supabase';
 
 const AuthService = {
-  login: async (email, token, password) => {
+  login: async (email, token) => {
     const { data, error } = await SupabaseClient.from('User')
       .select('*=')
       .eq('email', email)
-      .eq('oauth_token', token);
-
-    // const passwordMatch = await comparePassword(hashPassword(password), data[0].password);
-    // if (!passwordMatch) {
-    //   const error = new Error('Invalid Password');
-    //   error.statusCode = 403;
-    //   throw error;
-    // }
+      .eq('oauth_token', token)
 
     if (error) {
       throw error;
